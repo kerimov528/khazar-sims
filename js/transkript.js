bb7ee4894c2c90aa4e05a472064d347a15da98e1
kerimov528 <kerimov.anar528@gmail.com>
2022-09-08 09:29:02 -0700

    Fixed

--- js/transkript.js
+++ js/transkript.js
@@ -9,11 +9,11 @@ $(() => {
                 $("tbody#transkriptSubjects").html("");
                 $("table#tablo_ortalama1 tbody").html("");
                 $.each(data, (key, value) => {
-                    
-                    var name = localStorage.getItem('nameOfStudent');
-                    console.log(name,name.substring(0, name.indexOf(" ")));
 
-                    if (value.name == name.substring(0, name.indexOf(" ")) ) {
+                    var name = localStorage.getItem('nameOfStudent');
+                    console.log(name, name.substring(0, name.indexOf(" ")));
+
+                    if (value.name == name.substring(0, name.indexOf(" "))) {
                         for (let i = 0; i < value.subjects.length; i++) {
                             let str = "";
 
@@ -28,29 +28,29 @@ $(() => {
                                 (value.subjects[i].courseName == value.subjects[i + 1].courseName) :
                                 ((value.subjects[i].courseName == value.subjects[i - 1].courseName) && (value.subjects[i].grade > 60) && (value.subjects[i].ECTS != ""))
                             ) ?
-                            (
-                                (value.subjects[i].grade > value.subjects[i - 1].grade) ?
-                                (str += "<td class='align-middle secondChance'>" + value.subjects[i].courseName + "</td>") :
-                                (str += "<td class='align-middle bg-aquamarine firstChance'>" + value.subjects[i].courseName + "</td>")
-                            ) : (
-                                str += "<td class='align-middle'>" + value.subjects[i].courseName + "</td>"
-                            );
+                                (
+                                    (value.subjects[i].grade > value.subjects[i - 1].grade) ?
+                                        (str += "<td class='align-middle secondChance'>" + value.subjects[i].courseName + "</td>") :
+                                        (str += "<td class='align-middle bg-aquamarine firstChance'>" + value.subjects[i].courseName + "</td>")
+                                ) : (
+                                    str += "<td class='align-middle'>" + value.subjects[i].courseName + "</td>"
+                                );
 
                             str += "<td class='text-center align-middle'>" + value.subjects[i].lang + "</td>";
 
                             // checking if credit is exists
-                            (value.subjects[i].credit == "") ? 
-                            (str += "<td class='text-center align-middle'>" + value.subjects[i].credit + "</td>") :
-                            (str += "<td class='text-center align-middle'>" + Number(value.subjects[i].credit).toFixed(1) + "</td>");
+                            (value.subjects[i].credit == "") ?
+                                (str += "<td class='text-center align-middle'>" + value.subjects[i].credit + "</td>") :
+                                (str += "<td class='text-center align-middle'>" + Number(value.subjects[i].credit).toFixed(1) + "</td>");
 
                             //checking if ECTS is paid or not
                             (value.subjects[i].ECTS == "") ?
-                            (str += "<td class='text-center align-middle bg-dark text-white noEcts'>" + value.subjects[i].ECTS + "</td>") :
-                            (str += "<td class='text-center align-middle'>" + value.subjects[i].ECTS + "</td>");
+                                (str += "<td class='text-center align-middle bg-dark text-white noEcts'>" + value.subjects[i].ECTS + "</td>") :
+                                (str += "<td class='text-center align-middle'>" + value.subjects[i].ECTS + "</td>");
 
 
                             // checking if a subject grade under 60 
-                            ((value.subjects[i].grade < 60) && (value.subjects[i].ECTS != "")) ? (str += "<td class='text-center align-middle bg-aquamarine failGrade'>" + value.subjects[i].grade + "</td>") : (str += "<td class='text-center align-middle'>" + 100 + "</td>");
+                            ((value.subjects[i].grade < 60) && (value.subjects[i].ECTS != "")) ? (str += "<td class='text-center align-middle bg-aquamarine failGrade'>" + value.subjects[i].grade + "</td>") : (str += "<td class='text-center align-middle'>" + value.subjects[i].grade + "</td>");
                             str += "</tr>"
                             $("tbody#transkriptSubjects").html($("tbody#transkriptSubjects").html() + str);
                         }
@@ -68,13 +68,13 @@ $(() => {
                             $("table#tablo_ortalama1 tbody").html($("table#tablo_ortalama1 tbody").html() + str);
                         }
 
-                        $("tr td.failGrade , tr td.firstChance").siblings().addClass("bg-aquamarine"); 
-                        $("tr td.noEcts").siblings().addClass("bg-dark text-white"); 
+                        $("tr td.failGrade , tr td.firstChance").siblings().addClass("bg-aquamarine");
+                        $("tr td.noEcts").siblings().addClass("bg-dark text-white");
                     }
                 })
             }
         );
     };
 
-     
+
 })
